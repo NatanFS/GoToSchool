869feb9efe4b5090442363e939bf715e909d0231
Natan Felipe <natanlipf@gmail.com>
2021-09-09 17:37:39 -0300

    React Table criada

---
+++ TCC/login/static/login/react/columns.js
@@ -0,0 +1,18 @@
+const COLUMNS = [
+    {
+        Header: 'id',
+        accessor: 'id',
+    },
+    {
+        Header: 'Nome',
+        accessor: 'first_name',
+    },
+    {
+        Header: 'CPF',
+        accessor: 'email',
+    },
+    {
+        Header: 'E-mail',
+        accessor: 'country',
+    },
+]
